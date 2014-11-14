f56183917ed90756e461ac48f4986040978dad93
Jonathan Rich <jrich@richj-GHG2J02.ECS.local>
2014-11-14 15:07:58 -0500

    Adding the ability to have named routes and adding a few routes.

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -4,6 +4,11 @@ namespace App;
 
 class Routes extends \Cohuatl\Routes
 {
+    private $named = [
+        '@setup_save' => '/setup/save',
+        '@login' => '/auth/login'
+    ];
+
     public function __construct() {
         $this->routes['/'] = function ( $params, $config, $filter, $user ) {
             if(!(new \Cohuatl\Store('BlogConfig'))->hasNext()) {
@@ -16,5 +21,21 @@ class Routes extends \Cohuatl\Routes
         $this->routes['/setup'] = function ($params, $config, $filter, $user) {
             (new Setup\Setup($params, $config, $filter, $user))->index();
         };
+
+        $this->routes['/setup/save'] = function ($params, $config, $filter, $user) {
+            (new Setup\Setup($params, $config, $filter, $user))->save();
+        };
+
+        $this->routes['/auth/:action'] = function($params, $config, $filter, $user) {
+            (new Auth\Auth())->route($params, $config, $filter, $user);
+        };
+    }
+
+    public function map($route) {
+        if(isset($this->named[$route])) {
+            return $this->named[$route];
+        }
+
+        throw new \RuntimeException( 'Unknown route: ' . $route );
     }
 }
